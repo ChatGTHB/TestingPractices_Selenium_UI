20f7ee6c548f89eddb291e0790a240b4372ed851
ChatGTHB <cihat.kose@hotmail.com>
2024-02-22 16:20:43 +0100

    Edited

--- src/utility/BaseDriver.java
+++ src/utility/BaseDriver.java
@@ -13,10 +13,10 @@ public class BaseDriver {
 
     static {
 
-        Logger logger= Logger.getLogger(""); // Get output logs
+        Logger logger = Logger.getLogger(""); // Get output logs
         logger.setLevel(Level.SEVERE);            // Show only ERRORs
 
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize(); // It maximizes the screen.
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         // 30 sec delay: time to load the page
@@ -32,8 +32,16 @@ public class BaseDriver {
         // eğer 2 sn yüklerse 30 sn. beklemez.
     }
 
-    public static void waitAndClose(){
-      MyFunction.wait(3);
-      driver.quit();
+    public static void waitAndClose() {
+        MyFunction.wait(3);
+        driver.quit();
     }
+
+    //  Java fast - Website slow
+    //  It tries to find the java element, but the website is still loading, it says I couldn't find java before the site is loaded, and it ends,
+    //  When trying to find web element (FindElement/s) -->
+    //  Solution 1: we will give some time (20s)
+
+    //  Thread.sleep(); -> It stops Java directly for the given time. The more seconds you give, the more it stops the program. That's why it doesn't work for us.
+    //  For example, we gave him 20 seconds to exit when he finds us, we want him to find java at 10 seconds and exit, not to wait 20 seconds.
 }
