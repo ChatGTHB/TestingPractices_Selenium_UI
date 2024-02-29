a7f5c7ae87625438f8d3f5a9218c8b7fd8b609b2
ChatGTHB <cihat.kose@hotmail.com>
2024-02-29 16:55:00 +0100

    Edited

--- src/_04_XPath/Test04.java
+++ src/_04_XPath/Test04.java
@@ -10,46 +10,46 @@ import utility.BaseDriver;
 public class Test04 extends BaseDriver {
 
     @Test
-
     public void xpathTest() {
 
         driver.get("http://demo.nopcommerce.com/");
 
-        WebElement loginLink= driver.findElement(By.xpath("//a[@class='ico-login']"));
+        WebElement loginLink = driver.findElement(By.xpath("//a[@class='ico-login']"));
         //a[contains(@href,'login')]
         loginLink.click();
 
-        WebElement eMail= driver.findElement(By.xpath("//input[@id='Email']"));
+        WebElement eMail = driver.findElement(By.xpath("//input[@id='Email']"));
         // (//input[starts-with(@id,Email)])[3]
         eMail.sendKeys("adminTechno@gmail.com");
 
-        WebElement password= driver.findElement(By.xpath("//input[@name='Password']"));
+        WebElement password = driver.findElement(By.xpath("//input[@name='Password']"));
         //*[@name='Password' and @type='password']
         password.sendKeys("Admin123");
 
-        WebElement loginButton=driver.findElement(By.xpath("//button[text()='Log in']"));
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Log in']"));
         //button[@type='submit' and contains(@class,'1 login')]
         loginButton.click();
 
-        WebElement searchInput=driver.findElement(By.xpath("//input[@id='small-searchterms']"));
+        WebElement searchInput = driver.findElement(By.xpath("//input[@id='small-searchterms']"));
         //div[@class='search-box store-search-box']/form/input
         searchInput.sendKeys("Beats Pill");
 
-        WebElement searchButton= driver.findElement(By.xpath("//div[@class='search-box store-search-box']/form/button"));
+        WebElement searchButton = driver.findElement(By.xpath("//div[@class='search-box store-search-box']/form/button"));
         searchButton.click();
 
-        WebElement product=driver.findElement(By.xpath("//a[text()='Beats Pill 2.0 Wireless Speaker']"));
-        Assert.assertTrue(product.getText().contains("Beats Pill"));
+        WebElement product = driver.findElement(By.xpath("//a[text()='Beats Pill 2.0 Wireless Speaker']"));
+        Assert.assertTrue("Not confirmed", product.getText().contains("Beats Pill"));
+        Assert.assertTrue(product.isDisplayed());
         product.click();
 
-        WebElement addToCard = driver.findElement(By.xpath("//button[contains(@id,'add-to-cart')]"));
-        addToCard.click();
+        WebElement addToCart = driver.findElement(By.xpath("//button[contains(@id,'add-to-cart')]"));
+        addToCart.click();
 
-        WebElement shoppingCard = driver.findElement(By.xpath("//*[text()='shopping cart']"));
-        shoppingCard.click();
+        WebElement shoppingCart = driver.findElement(By.xpath("//a[text()='shopping cart']"));
+        shoppingCart.click();
 
-        WebElement confirmCard = driver.findElement(By.xpath("(//*[text()='Beats Pill 2.0 Wireless Speaker'])[2]"));
-        Assert.assertTrue(confirmCard.isEnabled());
+        WebElement confirmCart = driver.findElement(By.xpath("(//a[text()='Beats Pill 2.0 Wireless Speaker'])[2]"));
+        Assert.assertTrue(confirmCart.isDisplayed());
 
         waitAndClose();
     }
--- src/_05_Select_ElementInStatus/Test05.java
+++ src/_05_Select_ElementInStatus/Test05.java
@@ -8,33 +8,33 @@ import org.openqa.selenium.support.ui.Select;
 import utility.BaseDriver;
 import utility.MyFunction;
 
+
 public class Test05 extends BaseDriver {
 
     @Test
-    public void scenario2() {
+    public void scenario1() {
 
         driver.get("http://demo.nopcommerce.com/");
 
         WebElement registerLink = driver.findElement(By.linkText("Register"));
         registerLink.click();
 
-
         MyFunction.wait(1);
         WebElement genderMale = driver.findElement(By.id("gender-male"));
 
-
-        System.out.println("genderMale.isDisplayed() = " + genderMale.isDisplayed());
         System.out.println("genderMale.isEnabled() = " + genderMale.isEnabled());
+        System.out.println("genderMale.isDisplayed() = " + genderMale.isDisplayed());
         System.out.println("genderMale.isSelected() = " + genderMale.isSelected());
 
         genderMale.click();
+        Assert.assertTrue(genderMale.isSelected());
 
-        System.out.println("*****************************************************");
-        System.out.println("After check: ");
-        System.out.println("genderMale.isDisplayed() = " + genderMale.isDisplayed());
+        System.out.println("********************************");
+        System.out.println("After check");
+
         System.out.println("genderMale.isEnabled() = " + genderMale.isEnabled());
+        System.out.println("genderMale.isDisplayed() = " + genderMale.isDisplayed());
         System.out.println("genderMale.isSelected() = " + genderMale.isSelected());
-
 
         MyFunction.wait(1);
         WebElement firstName = driver.findElement(By.id("FirstName"));
@@ -46,20 +46,20 @@ public class Test05 extends BaseDriver {
 
         WebElement dayMenu = driver.findElement(By.xpath("//select[@name='DateOfBirthDay']"));
         Select daySelectMenu = new Select(dayMenu);
-        daySelectMenu.selectByIndex(4);
-        daySelectMenu.selectByValue("3");
-        daySelectMenu.selectByVisibleText("3");
+        daySelectMenu.selectByIndex(3);
+//        daySelectMenu.selectByValue("3");
+//        daySelectMenu.selectByVisibleText("3");
 
         WebElement monthMenu = driver.findElement(By.xpath("//select[@name='DateOfBirthMonth']"));
         Select monthSelectMenu = new Select(monthMenu);
-        monthSelectMenu.selectByIndex(4);
+//        monthSelectMenu.selectByIndex(4);
         monthSelectMenu.selectByValue("4");
-        monthSelectMenu.selectByVisibleText("April");
+//        monthSelectMenu.selectByVisibleText("April");
 
         WebElement yearMenu = driver.findElement(By.xpath("//select[@name='DateOfBirthYear']"));
         Select yearSelectMenu = new Select(yearMenu);
-        yearSelectMenu.selectByIndex(1);
-        yearSelectMenu.selectByValue("1920");
+//        yearSelectMenu.selectByIndex(7);
+//        yearSelectMenu.selectByValue("1920");
         yearSelectMenu.selectByVisibleText("1920");
 
         MyFunction.wait(1);
@@ -82,8 +82,9 @@ public class Test05 extends BaseDriver {
         WebElement registerButton = driver.findElement(By.className("register-next-step-button"));
         registerButton.click();
 
-        WebElement confirmMessage = driver.findElement(By.xpath("(//div[@class='result'])[1]"));
-        Assert.assertTrue(confirmMessage.isDisplayed());
+        WebElement confirmMessage = driver.findElement(By.xpath("//div[@class='result']"));
+        Assert.assertTrue("Not confirmed", confirmMessage.getText().contains("Your registration completed"));
+        // Assert.assertTrue(confirmMessage.isDisplayed());
 
         waitAndClose();
     }
