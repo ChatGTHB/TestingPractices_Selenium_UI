05d49b2ac6855459288665fd0c43baf8aa7f6cca
ChatGTHB <cihat.kose@hotmail.com>
2024-03-07 17:06:48 +0100

    Added new testing practices

---
+++ src/_09_Iframes_Windows_Scroll/Iframes.java
@@ -0,0 +1,39 @@
+package _09_Iframes_Windows_Scroll;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import src.utility.BaseDriver;
+
+public class Iframes extends BaseDriver {
+
+    @Test
+    public void iFrameTest() {
+
+        // 1. Sayfayı açmak için gerekli URL'yi kullanın
+        driver.get("https://the-internet.herokuapp.com/iframe");
+
+        // 2. Iframe'e geçmek için iframe'in id, name veya index özelliğini kullanın
+        WebElement iframe = driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']"));
+        driver.switchTo().frame(iframe);
+//        driver.switchTo().frame(0);
+//        driver.switchTo().frame("mce_0_ifr");
+
+        // 3. Iframe'in içindeki metin kutusunu bulmak için xpath kullanın
+        WebElement textArea = driver.findElement(By.xpath("//body[@id='tinymce']"));
+
+        // 4. Metin kutusunun içeriğini temizlemek için clear() metodunu kullanın
+        textArea.clear();
+
+        // 5. Metin kutusuna yeni bir metin yazmak için sendKeys() metodunu kullanın
+        textArea.sendKeys("Bu metin Selenium ile değiştirildi.");
+
+        // 6. Iframe'den çıkmak parentFrame() metodunu kullanın
+        driver.switchTo().parentFrame();
+
+        // 7. Tüm iframelerden çıkıp direkt ana sayfaya dönmek için defaultContent() metodunu kullanın
+        driver.switchTo().defaultContent();
+
+        waitAndClose();
+    }
+}
---
+++ src/_09_Iframes_Windows_Scroll/Windows.java
@@ -0,0 +1,43 @@
+package _09_Iframes_Windows_Scroll;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import src.utility.BaseDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class Windows extends BaseDriver {
+
+    @Test
+    public void windowsTest() {
+
+        driver.get("https://demoqa.com/browser-windows");
+
+        List<WebElement> consent = driver.findElements(By.xpath("//p[@class='fc-button-label']"));
+        if (!consent.isEmpty()) {
+            consent.get(0).click();
+        }
+
+        String firstTabID = driver.getWindowHandle();
+
+        WebElement newTab = driver.findElement(By.xpath("//button[@id='tabButton']"));
+        newTab.click();
+
+        Set<String> windowIDs = driver.getWindowHandles();
+
+        for (String windowID : windowIDs) {
+            if (windowID.equals(firstTabID)) {
+                continue;
+            }
+            driver.switchTo().window(windowID);
+        }
+
+        WebElement text = driver.findElement(By.xpath("//h1[@id='sampleHeading']"));
+
+        System.out.println("text.getText() = " + text.getText());
+
+        waitAndClose();
+    }
+}
