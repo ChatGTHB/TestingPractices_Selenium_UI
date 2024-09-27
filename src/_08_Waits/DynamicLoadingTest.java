d41b8cd634c97a88c2d5a989ac3d0bf477df1ece
ChatGTHB <cihat.kose@hotmail.com>
2024-09-27 20:52:15 +0200

    Update

--- src/_08_Waits/DynamicLoadingTest.java
+++ src/_08_Waits/DynamicLoadingTest.java
@@ -5,44 +5,41 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import utility.BaseDriver;
 
-import java.time.Duration;
-
-public class ExplicitWaitButtonExample extends BaseDriver {
+public class DynamicLoadingTest extends BaseDriver {
 
     /**
-     Task 3: Explicit Wait (Button Click)
-     Görev:
+     Task 3:
+     Explicit Wait (Button Click)
 
-     "https://the-internet.herokuapp.com/dynamic_loading/2" sitesine gidiniz.
-     Start butonuna tıklayınız.
-     Dinamik olarak yüklenecek Hello World! yazısını bekleyiniz ve yazının görünür olduğunu doğrulayınız.
+     Görev:
+     - "https://the-internet.herokuapp.com/dynamic_loading/2" sitesine gidiniz.
+     - Start butonuna tıklayınız.
+     - Dinamik olarak yüklenecek "Hello World!" yazısını bekleyiniz ve yazının görünür olduğunu doğrulayınız.
 
      Çözüm:
      Bu task'ta, Explicit Wait kullanarak dinamik olarak yüklenen bir elementin görünür olmasını bekleyeceğiz.
      */
-
     @Test
     public void explicitWaitButtonTask() {
         // 1. Adım: Web sitesine git
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/2");
 
-        // 2. Adım: "Start" butonuna tıkla
+        // 2. Adım: "Start" butonunu bulun ve tıklayın
         WebElement startButton = driver.findElement(By.xpath("//button[text()='Start']"));
         startButton.click();
 
-        // 3. Adım: Explicit Wait tanımla (10 saniye)
-        // WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        // 4. Adım: "Hello World!" yazısının görünmesini bekle
+        // 3. Adım: "Hello World!" yazısının görünmesini bekleyin (Explicit Wait ile)
         WebElement helloWorldText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h4[text()='Hello World!']")));
+
+        // 4. Adım: Yazının ekrana geldiğini konsola yazdır
         System.out.println("helloWorldText.getText() = " + helloWorldText.getText());
 
-        // 5. Adım: Yazının göründüğünü doğrula
-        Assert.assertTrue("'Hello World!' yazısı görünmedi!",helloWorldText.isDisplayed());
+        // 5. Adım: Yazının gerçekten göründüğünü doğrula
+        Assert.assertTrue("'Hello World!' yazısı görünmedi!", helloWorldText.isDisplayed());
 
-        waitAndClose();  // Tarayıcıyı kapat
+        // 6. Adım: Test bittikten sonra tarayıcıyı kapat
+        waitAndClose();
     }
 }
